e92ac09e665cfa399ee4aeb32f993edea7de3918
just3mhz <andrey.nikitin.98@yandex.ru>
2021-09-06 09:17:24 +0300

    Add cleaning temp files in indexer

--- bsbi/bsbi.cpp
+++ bsbi/bsbi.cpp
@@ -9,6 +9,14 @@ std::string makeFilename(std::size_t blockNum)
     return "block_" + std::to_string(blockNum) + ".txt";
 }
 
+template<class Iterator>
+void cleanTempFiles(Iterator begin, Iterator end)
+{
+    for (auto it = begin; it != end; ++it) {
+        std::remove(it->c_str());
+    }
+}
+
 }
 
 BlockedSortBasedIndexer::BlockedSortBasedIndexer(std::size_t blockSize)
@@ -25,6 +33,7 @@ void BlockedSortBasedIndexer::makeIndex(const std::string& inputFilePath, const 
     block.reserve(blockSize_);
 
     std::size_t blocksProcessed = 0;
+    std::vector<std::string> tempFiles;
 
     while (!reader.allDocsRead()) {
         for(int i = 0; i < blockSize_ && !reader.allDocsRead(); ++i) {
@@ -36,9 +45,15 @@ void BlockedSortBasedIndexer::makeIndex(const std::string& inputFilePath, const 
 
         ++blocksProcessed;
 
-        recordDumper_->dump(makeFilename(blocksProcessed), records);
+        tempFiles.push_back(makeFilename(blocksProcessed));
+        recordDumper_->dump(tempFiles.back(), records);
+
         block.clear();
     }
+
+    // TODO: Add merging files to postings
+
+    cleanTempFiles(tempFiles.begin(), tempFiles.end());
 
     auxiliary::SingletonDictionary::getInstance().dump("dict.json");
 }
