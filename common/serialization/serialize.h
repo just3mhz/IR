072a03496e06377bb53e3c6961e692118dbd1bca
just3mhz <andrey.nikitin.98@yandex.ru>
2021-09-10 15:56:10 +0300

    Deserialization and tests

--- common/serialization/serialize.h
+++ common/serialization/serialize.h
@@ -4,6 +4,11 @@
 
 namespace common::serialization {
 
+namespace {
+constexpr auto t_value = static_cast<std::uint8_t>('T');
+constexpr auto f_value = static_cast<std::uint8_t>('F');
+}
+
 class Serializable {
 public:
     virtual ~Serializable() = default;
@@ -24,9 +29,6 @@ auto write(std::ostream& os, const T& value) -> std::size_t
 template <>
 auto write(std::ostream& os, const bool& value) -> std::size_t
 {
-    constexpr auto t_value = static_cast<std::uint8_t>('T');
-    constexpr auto f_value = static_cast<std::uint8_t>('F');
-
     const auto pos = os.tellp();
     const auto tmp = (value) ? t_value : f_value;
     os.write(reinterpret_cast<const char*>(&tmp), sizeof(tmp));
@@ -49,6 +51,71 @@ auto write(std::ostream& os, const Serializable& value) -> std::size_t
 {
     return value.serialize(os);
 }
+
+template <class T>
+auto read(std::istream& is, T& value) -> std::size_t
+{
+    const auto pos = is.tellg();
+    is.read(reinterpret_cast<char*>(&value), sizeof(value));
+    return static_cast<std::size_t>(is.tellg() - pos);
+}
+
+template <>
+auto read(std::istream& is, bool& value) -> std::size_t
+{
+    const auto pos = is.tellg();
+
+    uint8_t tmp = 0;
+    is.read(reinterpret_cast<char*>(&tmp), sizeof(tmp));
+
+    if (tmp == t_value) {
+        value = true;
+    } else if (tmp == f_value) {
+        value = false;
+    } else {
+        // TODO: Logging or exception
+        std::cerr << "Unexpected byte: "
+                  << static_cast<uint32_t>(tmp)
+                  << "; Expected: "
+                  << static_cast<uint32_t>(t_value)
+                  << "or "
+                  << static_cast<uint32_t>(f_value);
+    }
+
+    return static_cast<std::size_t>(is.tellg() - pos);
+}
+
+template <>
+auto read(std::istream& is, std::string& value) -> std::size_t
+{
+    const auto pos = is.tellg();
+
+    std::uint32_t len;
+    is.read(reinterpret_cast<char*>(&len), sizeof(len));
+
+    std::string s;
+    s.resize(len);
+
+    if (len > 0)
+        is.read(s.data(), len);
+
+    auto bytesRead = static_cast<std::size_t>(is.tellg() - pos);
+    if (bytesRead < sizeof(len) + len) {
+        // TODO: Logging or exception
+        std::cerr << "Byte read: " << bytesRead << "; Expected: " << sizeof(len) + len << std::endl;
+    }
+
+    value = std::move(s);
+
+    return bytesRead;
+}
+
+template <>
+auto read(std::istream& is, Serializable& value) -> std::size_t
+{
+    return value.deserialize(is);
+}
+
 
 } // namespace common::serialization
 
---
+++ common/serialization/tests/tests.cpp
@@ -0,0 +1,115 @@
+#include "../serialize.h"
+
+#include <gtest/gtest.h>
+
+namespace {
+
+class SerializableClass final: public common::serialization::Serializable
+{
+public:
+    SerializableClass() = default;
+    SerializableClass(bool b, std::string s, int i)
+        : boolValue(b)
+        , stringValue(std::move(s))
+        , intValue(i)
+    {}
+
+    auto serialize(std::ostream& os) const -> std::size_t override {
+        std::size_t bytes = 0;
+        bytes += common::serialization::write(os, boolValue);
+        bytes += common::serialization::write(os, stringValue);
+        bytes += common::serialization::write(os, intValue);
+        return bytes;
+    }
+
+    auto deserialize(std::istream& is) -> std::size_t override {
+        std::size_t bytes = 0;
+        bytes += common::serialization::read(is, boolValue);
+        bytes += common::serialization::read(is, stringValue);
+        bytes += common::serialization::read(is, intValue);
+        return bytes;
+    }
+
+    auto serialized_size() const noexcept -> std::size_t override {
+        const auto len = static_cast<uint32_t>(stringValue.size());
+        return sizeof(boolValue) + sizeof(intValue) + (sizeof(len) + len);
+    }
+
+    bool operator==(const SerializableClass& other) const {
+        return boolValue == other.boolValue && stringValue == other.stringValue && intValue == other.intValue;
+    }
+
+private:
+    bool boolValue{true};
+    std::string stringValue{"Initial string"};
+    int intValue{-1};
+};
+
+}
+
+
+TEST(TestIntegralTypes, TestInt)
+{
+    std::stringstream s;
+
+    int32_t read = 0;
+    int32_t write = 123456;
+    common::serialization::write(s, write);
+    common::serialization::read(s, read);
+
+    ASSERT_EQ(read, write);
+}
+
+TEST(TestIntegralTypes, TestDouble)
+{
+    std::stringstream s;
+
+    double read = 0;
+    double write = 123456;
+    common::serialization::write(s, write);
+    common::serialization::read(s, read);
+
+    ASSERT_EQ(read, write);
+}
+
+TEST(TestSpecialTypes, TestBool)
+{
+    std::stringstream s;
+
+    bool read = true;
+    bool write = false;
+
+    common::serialization::write(s, write);
+    common::serialization::read(s, read);
+    ASSERT_EQ(read, write);
+
+    read = false;
+    write = true;
+    common::serialization::write(s, write);
+    common::serialization::read(s, read);
+    ASSERT_EQ(read, write);
+}
+
+TEST(TestSpecialTypes, TestString)
+{
+    std::stringstream s;
+
+    std::string write("Some useful string");
+    std::string read;
+    common::serialization::write(s, write);
+    common::serialization::read(s, read);
+    ASSERT_EQ(write, read);
+}
+
+TEST(TestSpecialTypes, TestSerializable)
+{
+    std::stringstream s;
+
+    SerializableClass obj1(false, "Test", 128);
+    SerializableClass obj2;
+    std::size_t bytes1 = common::serialization::write<common::serialization::Serializable>(s, obj1);
+    std::size_t bytes2 = common::serialization::read<common::serialization::Serializable>(s, obj2);
+
+    ASSERT_EQ(bytes1, bytes2);
+    ASSERT_EQ(obj1, obj2);
+}
