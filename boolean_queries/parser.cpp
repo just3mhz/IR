78782ce027d561383ff1c7304ddb4ddf91dc664b
just3mhz <andrey.nikitin.98@yandex.ru>
2021-09-23 20:43:32 +0300

    Refactor parsing

---
+++ boolean_queries/parser.cpp
@@ -0,0 +1,81 @@
+#include "parser.h"
+
+namespace boolean_queries {
+
+namespace {
+
+void processOperator(
+    const std::shared_ptr<Token>& token,
+    std::vector<std::shared_ptr<Token>>& outputTokens,
+    std::stack<std::shared_ptr<Token>>& operators)
+{
+    const auto& op1 = std::dynamic_pointer_cast<Operator>(token);
+    while (!operators.empty()) {
+        if (operators.top()->tokenType() == TokenType::BRACKET) {
+            break;
+        }
+        const auto op2 = std::dynamic_pointer_cast<Operator>(operators.top());
+        if (op2->priority() < op1->priority()) {
+            break;
+        }
+        outputTokens.push_back(op2);
+        operators.pop();
+    }
+    operators.push(op1);
+}
+
+void processBracket(
+    const std::shared_ptr<Token>& token,
+    std::vector<std::shared_ptr<Token>>& outputTokens,
+    std::stack<std::shared_ptr<Token>>& operators)
+{
+    const auto bracket = std::dynamic_pointer_cast<Bracket>(token);
+
+    if (bracket->isOpen()) {
+        operators.push(bracket);
+        return;
+    }
+
+    while (!operators.empty() && operators.top()->tokenType() != TokenType::BRACKET) {
+        outputTokens.push_back(operators.top());
+        operators.pop();
+    }
+
+    if (operators.empty())
+        throw std::runtime_error("Unable to parse expression");
+    operators.pop();
+}
+
+} // namespace
+
+std::vector<std::shared_ptr<Token>> reversePolishNotation(const std::vector<std::shared_ptr<Token>>& tokens)
+{
+    std::vector<std::shared_ptr<Token>> outputTokens;
+    std::stack<std::shared_ptr<Token>> operators;
+
+    for (const auto& token : tokens) {
+        switch (token->tokenType()) {
+        case TERM:
+            outputTokens.push_back(token);
+            break;
+        case OPERATOR:
+            processOperator(token, outputTokens, operators);
+            break;
+        case BRACKET:
+            processBracket(token, outputTokens, operators);
+            break;
+        }
+        throw std::runtime_error("Unknown TokenType");
+    }
+
+    while (!operators.empty()) {
+        if (operators.top()->tokenType() == TokenType::BRACKET)
+            throw std::runtime_error("Unable to parse expression");
+        outputTokens.push_back(operators.top());
+        operators.pop();
+    }
+
+    return outputTokens;
+}
+
+} // namespace boolean_queries
--- boolean_queries/parser.h
+++ boolean_queries/parser.h
@@ -9,53 +9,6 @@
 
 namespace boolean_queries {
 
-std::vector<std::shared_ptr<Token>> reversePolishNotation(const std::vector<std::shared_ptr<Token>>& tokens)
-{
-    std::vector<std::shared_ptr<Token>> outputTokens;
-    std::stack<std::shared_ptr<Token>> operators;
+std::vector<std::shared_ptr<Token>> reversePolishNotation(const std::vector<std::shared_ptr<Token>>& tokens);
 
-    for(const std::shared_ptr<Token>& token: tokens) {
-        if (token->tokenType() == TokenType::TERM) {
-            outputTokens.push_back(token);
-        } else if (token->tokenType() == TokenType::OPERATOR) {
-            const auto op1 = std::dynamic_pointer_cast<Operator>(token);
-            while (!operators.empty()) {
-                if (operators.top()->tokenType() == TokenType::BRACKET) {
-                    break;
-                }
-                const auto op2 = std::dynamic_pointer_cast<Operator>(operators.top());
-                if (op2->priority() < op1->priority()) {
-                    break;
-                }
-                outputTokens.push_back(op2);
-                operators.pop();
-            }
-            operators.push(op1);
-        } else if (token->tokenType() == TokenType::BRACKET) {
-            const auto bracket = std::dynamic_pointer_cast<Bracket>(token);
-            if (bracket->isOpen()) {
-                operators.push(bracket);
-            } else {
-                while(!operators.empty() && operators.top()->tokenType() != TokenType::BRACKET) {
-                    outputTokens.push_back(operators.top());
-                    operators.pop();
-                }
-
-                if (operators.empty())
-                    throw std::runtime_error("Unable to parse expression");
-                operators.pop();
-            }
-        }
-    }
-
-    while (!operators.empty()) {
-        if (operators.top()->tokenType() == TokenType::BRACKET)
-            throw std::runtime_error("Unable to parse expression");
-        outputTokens.push_back(operators.top());
-        operators.pop();
-    }
-
-    return outputTokens;
-}
-
-}
+} // namespace boolean_queries
